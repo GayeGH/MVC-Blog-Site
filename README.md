67331172a894cc4cf7d4f88d0f7aac18d3b77151
Annelle <gghaslam@gmail.com>
2023-02-08 15:34:23 -0500

    README file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Title
+MCC BLOG SITE
+
+## Description
+
+MvC Blog Site is a blog website where users can post their thoughts about different tech articles.
+
+## Installation
+
+Express, Handlebars, Sequelize, Node, Mysql2
+
+## Usage
+
+Users will be able to login and post thoughts about an article they have recently read about technology. Then they will be able to logout and revisit the site to view other posts from different viewers. 
+
+
+## License
+"ISC"
+
+## Contact
+gghaslam@gmail.com
+
--- public/js/login.js
+++ public/js/login.js
@@ -3,33 +3,36 @@ const loginFormHandler = async (event) => {
   
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-  
+    
+
     if (email && password) {
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
-      if (response.ok) {
+       if (response.ok) {
         document.location.replace('/');
       } else {
         alert('Failed to log in.');
       }
-    }
-  };
+    };
+  
   
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
+    const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    console.log('name', name)
+    console.log("email", email)
+    console.log('password', password)
   
-    if (username && email && password) {
+    if (name && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -48,4 +51,4 @@ const loginFormHandler = async (event) => {
   document
     .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler);
-  
+}
