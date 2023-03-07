b63bd8f2e09ce5fe904ee926cb0475e8b597fe69
Annelle <gghaslam@gmail.com>
2023-03-07 15:55:43 -0500

    User has many and blogposts belong to code added

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
-const router = require('express').Router();
 const User = require('./User');
 const Blogposts = require('./Blogposts')
+
+User.hasMany(Blogposts, {
+    foreignKey: 'user_id',
+    onDelete: 'Cascade'
+});
+
+Blogposts.belongsTo(User, {
+    foreignKey: 'user_id'
+}):
+
 module.exports = { User, Blogposts };
